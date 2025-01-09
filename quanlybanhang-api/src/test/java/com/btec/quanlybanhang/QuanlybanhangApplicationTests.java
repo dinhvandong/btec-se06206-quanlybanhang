c6e5804e69ec92de798eb4b0a0fca07ff6a6088f
dinhvandong <dongvan.ict3@gmail.com>
2025-01-09 16:55:16 +0700

    first commit
     first commit my source

---
+++ quanlybanhang-api/src/test/java/com/btec/quanlybanhang/QuanlybanhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btec.quanlybanhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlybanhangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
